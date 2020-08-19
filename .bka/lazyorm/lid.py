e54755382018dd31fd7ba2fe333dfca4e928c68b
XiangYu Ye <yexiangyu@gmail.com>
2020-08-19 21:38:23 +0800

    remove legcy code

--- .bka/lazyorm/lid.py
+++
@@ -1,7 +0,0 @@
-from nanoid import generate
-
-ALPHABET = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-
-
-def lid(size=32):
-    return generate(ALPHABET, size)
--- .bka/lazyorm/lnode.py
+++
@@ -1,3 +0,0 @@
-from .lnode_elastic import ESNode, init_elastic
-from .lnode_mqtt import MQTTNode, init_mqtt
-from .lnode_redis import RedisNode, init_redis
